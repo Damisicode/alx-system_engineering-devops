c1996979ab38fc77bdf158d9459e77d4704fec84
Oluwadamisi Ayinde <98517044+Damisicode@users.noreply.github.com>
2023-03-25 05:19:40 +0100

    Adding solutions tasks

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,4 @@
 # This is a file containing puppet code to create a file
-# File path is /tmp/school
-# File permission is 0744
-# File owner is www-data
-# File group is www-data
-# File contains I love Puppet
 file { '/tmp/School':
      ensure => file,
      path => '/tmp/School'
