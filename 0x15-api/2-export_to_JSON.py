b83561e877385f17af63741772688b1c55cea736
Ayinde Oluwadamisi <oluwadamisiayinde@gmail.com>
2023-04-24 21:43:44 -0700

    Adding Solutions to tasks

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -5,6 +5,7 @@ Record all tasks that owned by the employee.
 Format must be: { "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, {"task": "TASK_TITLE", "completed": "TASK_COMPLETED_STATUS", "username": "USERNAME"}, ... ]}
 File name must be: USER_ID.json
 """
+import json
 import requests
 from sys import argv
 
