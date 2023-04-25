2e437b29452d1972e70ba806ecc96abca7e32777
Ayinde Oluwadamisi <oluwadamisiayinde@gmail.com>
2023-04-24 21:41:35 -0700

    Adding Solutions to tasks

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -19,9 +19,15 @@ if __name__ == "__main__":
     for task in r:
         if (task.get("userId") == int(usr_id)):
             temp = []
-            temp.extend((usr_id, username, task.get("completed"), task.get("title")))
+            temp.extend((usr_id,
+                         username,
+                         task.get("completed"),
+                         task.get("title")))
             all_tasks.append(temp)
 
     with open("{}.csv".format(usr_id), 'w+') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer = csv.writer(csvfile,
+                            delimiter=',',
+                            quotechar='"',
+                            quoting=csv.QUOTE_ALL)
         writer.writerows(all_tasks)
