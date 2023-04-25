acd2dd8902c12987d959c4b64449ebfad993c3e1
Ayinde Oluwadamisi <oluwadamisiayinde@gmail.com>
2023-04-24 21:39:07 -0700

    Adding Solutions to tasks

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Extend the python script from exervise 0 to export daa in CSV format.
+Extend the python script from exercise 0 to export data in CSV format.
 Record all tasks that owned by the employee.
 Format must be: "USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"
 Field name must be: USER_ID.csv
@@ -11,16 +11,17 @@ from sys import argv
 
 if __name__ == "__main__":
     usr_id = argv[1]
-    username = requests.get("http://jsonplaceholder.typicode.com/users/{}".format(usr_id)).json().get("username")
+    username = requests.get("http://jsonplaceholder.typicode.com/users/{}"
+                            .format(usr_id)).json().get("username")
     all_tasks = []
     r = requests.get("http://jsonplaceholder.typicode.com/todos").json()
 
     for task in r:
         if (task.get("userId") == int(usr_id)):
             temp = []
-            temp.extend((eid, username, task.get("completed"), task.get("title")))
-        all_tasks.append(temp)
+            temp.extend((usr_id, username, task.get("completed"), task.get("title")))
+            all_tasks.append(temp)
 
     with open("{}.csv".format(usr_id), 'w+') as csvfile:
-        writer = csv.writer(csvfile, delimiter=', ', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
         writer.writerows(all_tasks)
