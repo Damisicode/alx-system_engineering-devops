a6852c0c7a6d5c0d9405c058ff994e7deaf58e21
Damisicode <98517044+Damisicode@users.noreply.github.com>
2023-07-09 20:47:09 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,5 +0,0 @@
-# Use Strace to find why apache is returning a 500 error
-exec { 'fix-apache':
-	command => "sed -i 's/phpp/php/g' /var/www/html/wp-setting.php",
-	path 	=> ['/usr/bin', '/usr/sbin', '/bin']
-}
