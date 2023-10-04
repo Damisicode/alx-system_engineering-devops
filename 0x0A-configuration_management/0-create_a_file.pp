e1d3aa834620a5ec63d20b91d48d4fab6bae48be
Damisicode <98517044+Damisicode@users.noreply.github.com>
2023-10-04 07:46:24 +0100

    Delete 0x0A-configuration_management directory

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# This is a file containing puppet code to create a file
-file { '/tmp/School':
-  ensure => file,
-  path => '/tmp/School'
-  mode => '0744'
-  owner => 'www-data',
-  group => 'www-data',
-  content => 'I love puppet'
-}
